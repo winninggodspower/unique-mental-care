042f45a24ad4393f3a14142ea8c0e325d5810b1a
winning godspower <winninggodspower@gmail.com>
2025-11-04 15:49:39 +0100

    fix: add firebase user locals variable to env.d.ts file

--- src/env.d.ts
+++ src/env.d.ts
@@ -13,3 +13,13 @@ interface ImportMetaEnv {
 interface ImportMeta {
     readonly env: ImportMetaEnv;
 }
+
+declare namespace App {
+  interface Locals {
+    user: { 
+        id: string; 
+        email: string; 
+        displayName: string | null;
+    } | null | undefined; 
+  }
+}
